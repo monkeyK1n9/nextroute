e70ceefb95a83f8fc448074fefd44a7646e1a506
monkeyK1n9 <mkeyk1n9@gmail.com>
2023-10-30 11:38:27 +0100

    create and delete new todo functions

---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,51 @@
+import { NextResponse } from "next/server";
+
+const DATA_SOURCE: string = "https://jsonplaceholder.typicode.com/todos";
+const API_KEY: string = process.env.DATA_API_KEY as string;
+
+export async function GET() {
+    const response = await fetch(DATA_SOURCE);
+
+    const todos: Todo[] = await response.json();
+
+    return NextResponse.json(todos);
+}
+
+export async function DELETE(request: Request) {
+    const { id }: Partial<Todo> = await request.json();
+
+    if (!id) return NextResponse.json({ message: 'Id is required' });
+
+    await fetch(`${DATA_SOURCE}/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'API-Key': API_KEY
+        }
+    });
+
+    return NextResponse.json({ message: `Todo with id ${id} deleted successfully` });
+}
+
+export async function POST(request: Request) {
+    const { userId, title }: Partial<Todo> = await request.json();
+
+    if (!userId || !title) return NextResponse.json({ message: 'Missing required data' });
+
+    const res = await fetch(`${DATA_SOURCE}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'API-Key': API_KEY
+        },
+        body: JSON.stringify({
+            userId,
+            title,
+            completed: false
+        })
+    });
+
+    const todo: Todo = await res.json();
+
+    return NextResponse.json(todo);
+}

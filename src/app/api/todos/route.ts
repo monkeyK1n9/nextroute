9fcde84caae3248c930a1cfc9e39d778d5b369f9
monkeyK1n9 <mkeyk1n9@gmail.com>
2023-11-03 15:22:10 +0100

    middleware and request limitation

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,12 +1,34 @@
 import { NextResponse } from "next/server";
+import { limiter } from "../config/limiter";
 
 const DATA_SOURCE: string = "https://jsonplaceholder.typicode.com/todos";
 const API_KEY: string = process.env.DATA_API_KEY as string;
 
-export async function GET() {
+export async function GET(request: Request) {
     const response = await fetch(DATA_SOURCE);
 
+    const origin = request.headers.get('origin');
+
     const todos: Todo[] = await response.json();
+
+    /**
+     * We use the limiter here to restrict the number of time the user can
+     * get the resource from the server. This is a useful trick.
+     * When the remaining tokens is negative we give a different response
+     */
+    const remainingTokens = await limiter.removeTokens(1)
+    console.log("Remaining tokens: ", remainingTokens);
+
+    if (remainingTokens < 0) {
+        return new NextResponse(null, {
+            status: 429,
+            statusText: "You exceeded the limit",
+            headers: {
+                'Access-Control-Allow-Origin': origin || "*", //you could also obtain the origin from the request or any other like thunderclient. Since origin was validated in the middleware(see middleware file), we allow this response to the valid origin
+                'Content-Type': "text/plain"
+            }
+        })
+    }
 
     return NextResponse.json(todos);
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,34 @@
 import { NextResponse } from "next/server";
 
+//we will use this to check if the origin of the request is valid before accepting it
+const allowedOrigins = process.env.NODE_ENV === "production" 
+    ? ["https://www.yoursite.com", "https://yoursite.com"] :
+    ["http://localhost:3000"]
+
 export function middleware(request: Request) {
+    const origin = request.headers.get('origin');
+
+    if (request.url.includes('/api/')) {
+        console.log('API route')
+    }
+
+    if (origin && !allowedOrigins.includes(origin)) {
+        //we block non-allowed origins
+        return new NextResponse(null, {
+            status: 400,
+            statusText: "Bad request",
+            headers: {
+                "Content-Type": "text/plain"
+            }
+        })
+    }
+    
     console.log(request.method)
     console.log(request.body)
-    
+
     return NextResponse.next();
+}
+
+export const config = {
+    matcher: "/api/:path*" //this is matcher config to make the middleware work for specific paths. see https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 }
